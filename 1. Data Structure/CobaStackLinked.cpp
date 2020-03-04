221f386df4b2903cd83f9f655fbcbb141033dae4
BSNugroho <bagus.s.n@mail.ugm.ac.id>
2020-03-04 23:22:27 +0700

    Days 2

--- 1. Data Structure/CobaStackLinked.cpp
+++ 1. Data Structure/CobaStackLinked.cpp
@@ -12,14 +12,45 @@ class linkedstack{
         node *tail=NULL;
     public:
         void newnode(int angka){
-            node *tmp;
+            node *tmp=new node;
             tmp->data=angka;
+            tmp->next=NULL;
             if(head==NULL){
                 head=tmp;
                 tail=tmp;
             }
-        }
-        void push(){
+            else{
+                tail->next=tmp;
+                tail=tail->next;
+            }
             
         }
+        void push(int number){
+            newnode(number);
+        }
+
+        void pop(){
+            node *tmp;
+            int tempor,count;
+            int *ptempor=new int;
+            tmp=head;
+            while(tmp!=NULL){
+                tempor=tmp->data;
+                ptempor=&tempor;
+                ptempor++;
+                count++;
+                tmp=tmp->next;
+            }
+            for (int i=0;i<count;i++){
+                cout<<*(--ptempor)<<"-";
+            }
+        }   
 };
+
+int main (){
+    linkedstack a;
+    a.push(5);
+    a.push(6);
+    a.push(7);
+    a.pop();
+}
